4564de53ad610b47dd6a8b98d4b1a29536875864
Amir hossein Karimi <amir.hossein7464@gmail.com>
2020-07-30 10:29:18 +0430

    Setting-up Laravel-Mix

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+let mix = require("laravel-mix");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix
+  .js("src/scripts/main.js", "dist/scripts/")
+  .sass("src/styles/main.scss", "dist/styles/")
+  .options({
+    autoprefixer: {
+      options: {
+        browsers: [">0.2%", "not dead", "not op_mini all"],
+      },
+    },
+  });
